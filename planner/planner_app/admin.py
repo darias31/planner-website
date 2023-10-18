2c7bd13fe4d6a29a5110c0a06b532c4ad4b049d7
Daria Stepanova <dashastepanova0731@gmail.com>
2023-10-18 14:57:58 -0500

    Redefined database structure

--- planner/planner_app/admin.py
+++ planner/planner_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Exercise, Workout, ExerciseInWorkout
+from .models import Exercise, Workout, ExerciseInWorkout, MuscleCategory, Muscle
 
 admin.site.register(Exercise)
 admin.site.register(Workout)
 admin.site.register(ExerciseInWorkout)
+admin.site.register(MuscleCategory)
+admin.site.register(Muscle)
--- planner/planner_app/models.py
+++ planner/planner_app/models.py
@@ -1,32 +1,47 @@
 from django.db import models
 import uuid
 
-class Exercise(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+class MuscleCategory(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    primary_muscle = models.CharField(max_length=100)
-    secondary_muscle = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.name
 
+class Muscle(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(MuscleCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Exercise(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    primary_muscle = models.ManyToManyField(Muscle, related_name='primary_exercises')
+    secondary_muscle = models.ManyToManyField(Muscle, related_name='secondary_exercises')
+
+    def __str__(self):
+        return self.name
+    
 class Workout(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     weekday = models.CharField(max_length=20)
     exercises = models.ManyToManyField(Exercise, through='ExerciseInWorkout')
 
     def __str__(self):
-        return f"{self.weekday} - {self.name}"
+        return self.name
 
 class ExerciseInWorkout(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
-    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
     workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     sets = models.PositiveIntegerField()
     reps = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"{self.exercise.name} - Sets: {self.sets}, Reps: {self.reps}"
+        return f'{self.exercise.name} in {self.workout.name}'
 
 
